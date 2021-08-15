53a8d6a4e2f498170832cab13f0df375d98bbd28
Feniksdv <feniksdv@gmail.com>
2021-08-15 21:16:14 +0300

    home work 5 3. Создал фейковые даные и залил их в БД. ВОПРОС Почему неполучается изменить локализацию в app.php Добавил ru_RU пробовал еще в class MessagesSeeder делать так         $faker = Factory::create('ru_RU'); все равно английский язык

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UsersSeeder::class,
+            StatusSeeder::class,
+            CustomersSeeder::class,
+            CategoriesSeeder::class,
+            NewsSeeder::class,
+            PagesSeeder::class,
+            OrdersSeeder::class,
+            MessagesSeeder::class
+        ]);
     }
 }
